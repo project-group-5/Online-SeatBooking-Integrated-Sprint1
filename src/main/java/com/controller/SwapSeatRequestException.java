385961e1f9ce5d4c4f6240ff949ff3f5726ec027
vinod-8152 <101784582+vinod-8152@users.noreply.github.com>
2022-04-18 13:26:49 +0530

    Add files via upload

---
+++ src/main/java/com/controller/SwapSeatRequestException.java
@@ -0,0 +1,13 @@
+package com.controller;
+public class SwapSeatRequestException  extends RuntimeException{
+	
+	public SwapSeatRequestException() {
+		super("Swap Request Cannot be sent");	
+		}
+
+		@Override
+		public String toString() {
+			return "Swap Request Cannot  be sent";
+		}
+
+}
